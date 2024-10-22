37c33addd887cbfd4d1fc135cbd35675badc944d
gustavo <gustavo.silva9@ua.pt>
2024-10-22 13:19:47 +0100

    Input básico

--- front-end/frontend/src/App.js
+++ front-end/frontend/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import EmailVerifier from './components/EmailVerifier'; // Importar o teu componente
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Email Verifier</h1>
+      <EmailVerifier />
     </div>
   );
 }
---
+++ front-end/frontend/src/components/EmailVerifier.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function EmailVerifier() {
+  return (
+    <div>
+      <input 
+        type="email" 
+        placeholder="Enter email" 
+      />
+    </div>
+  );
+}
+
+export default EmailVerifier;
